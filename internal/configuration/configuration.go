8dc91ae4bc7e3939d6d1f37df78ac72207556db7
Alex (@enenumxela) <62714471+enenumxela@users.noreply.github.com>
2024-06-13 15:59:27 +0300

    refactor(banner): Update the CLI banner

--- internal/configuration/configuration.go
+++ internal/configuration/configuration.go
@@ -9,14 +9,13 @@ const (
 
 var BANNER = aurora.Sprintf(
 	aurora.BrightBlue(` 
-          _
-__  _____| |_ _ __ ___  __ _ _ __ ___
-\ \/ / __| __| '__/ _ \/ _`+"`"+` | '_ `+"`"+` _ \
- >  <\__ \ |_| | |  __/ (_| | | | | | |
-/_/\_\___/\__|_|  \___|\__,_|_| |_| |_|
-                                 %s
+                      _
+            __  _____| |_ _ __ ___  __ _ _ __ ___
+            \ \/ / __| __| '__/ _ \/ _`+"`"+` | '_ `+"`"+` _ \
+             >  <\__ \ |_| | |  __/ (_| | | | | | |
+            /_/\_\___/\__|_|  \___|\__,_|_| |_| |_| %s
 
-       %s`).Bold(),
+                %s`).Bold(),
 	aurora.BrightRed("v"+VERSION).Bold(),
-	aurora.BrightYellow("with <3 by Hueristiq Open Source").Italic(),
+	aurora.BrightYellow("...with <3 by Hueristiq (https://github.com/hueristiq)").Italic(),
 )
